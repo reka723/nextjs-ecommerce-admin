1bd56a411b3fbd869c7e937d1888321228da0d7a
dobanyr <dobany.reka@hclinear.hu>
2023-08-16 10:18:08 +0200

    [FEAT] added mongo db

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,5 @@
+import clientPromise from "@/lib/mongodb";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
@@ -13,8 +15,13 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     // ...add more providers here
   ],
+  adapter: MongoDBAdapter(clientPromise),
 };
 
 export default NextAuth(authOptions);
--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,37 @@ export default function Home() {
   const { data: session } = useSession();
   if (!session) {
     return (
-      <div className="bg-blue-950 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
+      <div className="bg-gradient-to-r from-teal-800 to-teal-500 w-screen h-screen flex items-center justify-center ">
+        <div className="text-center flex flex-col gap-5 bg-teal-50 p-20 rounded-xl">
+          <h2 className="text-4xl text-teal-900">Welcome!</h2>
           <button
-            className="bg-white p-2 rounded-lg px-4"
+            className=" p-2 rounded-lg px-4 bg-teal-800 hover:ease-in duration-100 text-teal-50 hover:bg-teal-700"
             onClick={() => signIn("google")}
           >
             Login with Google
+          </button>
+          <button
+            className="bg-teal-800 p-2 rounded-lg px-4 hover:ease-in duration-100 text-teal-50 hover:bg-teal-700"
+            onClick={() => signIn("github")}
+          >
+            Login with GitHub
           </button>
         </div>
       </div>
     );
   }
-  return <div> logged in {session.user.email}</div>;
+
+  return (
+    <div className="bg-gradient-to-r from-teal-800 to-teal-500 w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+        <h2 className="text-white text-lg">Welcome {session.user.email}!</h2>
+        <button
+          className="bg-white p-2 rounded-lg px-4"
+          onClick={() => signOut()}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 }
